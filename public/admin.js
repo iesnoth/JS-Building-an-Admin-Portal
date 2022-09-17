6d10d214dfc9f2347c12d58e1d6901c6c70b0069
Victoria Beasley <iesnoth91@gmail.com>
2022-09-16 19:25:18 -0500

    added submit buttons

--- public/admin.js
+++ public/admin.js
@@ -16,28 +16,33 @@ async function getList() {
 // Display a list of book titles to the admin.
 //I need to create a function to get each title out of the array and individual object,
 //write HTML to make an input field and submit button next to each title
-//when a new quantity is put in, have the submit button dynamically send information from the admin site to the front site
-function bookAdmin(book){
+//when a new quantity is put in, have the submit button
+//dynamically send information from the admin site to the front site
+//this will be a click event and PATCH request
+function bookAdmin(book) {
     //creating a div for each book
     let bookEntry = document.createElement("div")
     //calling and appending the book titles
     let bookTitle = document.createTextNode(`${book.title}`)
     bookEntry.append(bookTitle)
-    //
+    //making inputs
     let bookInput = document.createElement("input")
-    bookInput.setAttribute('type','number')
-    bookInput.setAttribute('id','book-input')
+    bookInput.setAttribute('type', 'number')
+    bookInput.setAttribute('id', 'book-input')
     bookEntry.append(bookInput)
-    //document.getElementById('book-input').value = `${book.quantity}`;
-    //let bookQuantity = `${book.quantity}`
+    //SUBMIT BUTTONS
+    let submit = document.createElement("button")
+    let text = document.createTextNode("Submit")
+    submit.appendChild(text)
+    bookEntry.append(submit)
 
-    
+
+
     //appending div
     document.body.append(bookEntry)
- }
+}
 
 // Give each text input a value: the quantity of the associated book.
-// Place a submit button next to each text input.
 // When the submit button is clicked, retrieve the quantity
 //from the associated text input and save the updated quantity to the server.
 
